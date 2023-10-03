050930fda7969288338bfafc8691ed49c5381302
Iheb Chaoued <ihebchaoued@gmail.com>
2023-10-03 20:26:44 +0000

    another comb

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for i in range(0, 10):
     for j in range(i + 1, 10):
         if i != j:
             if i == 8 and j == 9:
-                print("{:02d}".format(int(str(i) + str(j)))
+                print("{}{}".format(i, j))
             else:
-                print("{:02d}, ".format(int(str(i) + str(j))), end="")
+                print("{}{}, ".format(i, j), end="")
