8da7b8b256c4f2de2bba45356467b05c7c4ae300
Iheb Chaoued <ihebchaoued@gmail.com>
2023-10-03 20:25:40 +0000

    another comb

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,7 @@
 for i in range(0, 10):
     for j in range(i + 1, 10):
         if i != j:
-            print("{:02d}".format(int(str(i) + str(j))), end=", ")
+            if i == 8 and j == 9:
+                print("{:02d}".format(int(str(i) + str(j)))
+            else:
+                print("{:02d}, ".format(int(str(i) + str(j))), end="")
