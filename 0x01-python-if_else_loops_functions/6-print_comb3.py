b51d8aafeb5733d3e3cc263ac6cb1c78763d0660
Iheb Chaoued <ihebchaoued@gmail.com>
2023-10-03 20:02:56 +0000

    another comb

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(10):
+for i in range(1, 10):
     for j in range(i + 1, 10):
         if i != j:
-            if (i != 0 and j != 1):
+            if i != 0 and j != 1:
                 print(", ", end="")
             print("{:d}{:d}".format(i, j), end="")
