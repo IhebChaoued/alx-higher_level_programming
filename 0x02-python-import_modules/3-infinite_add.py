6c47a8d6110add4ab41aad4612b30a2d74bad73b
Iheb Chaoued <ihebchaoued@gmail.com>
2023-10-05 14:37:19 +0000

    ADD ADD ADD....

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    from sys import argv
+
+    total = 0
+    for arg in argv [1:]:
+        total += int(arg)
+        print(total)
