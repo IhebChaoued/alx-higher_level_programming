ce817250aeb779ee65465c8e660f9a1caeae058d
Iheb Chaoued <ihebchaoued@gmail.com>
2023-10-05 15:43:24 +0000

    Hello there

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Print EVERYTHING"""
+    import hidden_4
+    
+    file = dir(hidden_4)
+    for i in file:
+        if i[:2] != "__":
+            print(i)
