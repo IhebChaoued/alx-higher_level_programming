e7c6a37998fc46404392a094bcf6b83a3ca7dbd7
Iheb Chaoued <ihebchaoued@gmail.com>
2023-10-08 15:45:06 +0000

    matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for line in matrix:
+        for i, j in enumerate(line):
+            if i == len(line) - 1:
+                print("(:d)".format(j))
+            else:
+                print("{:d}".format(j), end="")
+
+    if not matrix:
+        print()
