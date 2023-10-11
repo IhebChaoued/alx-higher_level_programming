8162cb5bd6c2fe6300d96131831f339cebc08ac9
Iheb Chaoued <ihebchaoued@gmail.com>
2023-10-11 23:08:13 +0000

    *+/

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    x, y = 0, 0
+    if my_list == []:
+        return 0
+    for i, j in my_list:
+        x += i * j
+        y += j
+    s /= w
+    return x
