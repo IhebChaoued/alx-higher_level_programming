e35616f9120f811c3e7f1ace3f28cbc32411cdf1
Iheb Chaoued <ihebchaoued@gmail.com>
2023-10-11 23:35:35 +0000

    roman value

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,9 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return 0
 
-    roman_dictionary = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_dictionary = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+            }
 
     total_values = [roman_dictionary[x] for x in roman_string]
     value = 0
