2d97f7df150e55680e04a3aa6b500e2b75286877
Iheb Chaoued <ihebchaoued@gmail.com>
2023-10-24 13:29:55 +0000

    last square

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""check"""
+
+
+class Square:
+    """check"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    def size(self):
+        return self.__size
+
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def position(self):
+        return self.__position
+
+    def position(self, value):
+        if not (isinstance(value, tuple) and len(value) == 2 and
+                all(isinstance(x, int) and x >= 0 for x in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__position[1]):
+                print()
+            for _ in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
