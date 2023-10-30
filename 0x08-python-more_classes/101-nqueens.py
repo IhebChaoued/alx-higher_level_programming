4cfa551e31650b6a1884742705d477a314679f5d
Iheb Chaoued <ihebchaoued@gmail.com>
2023-10-30 16:09:14 +0000

    Advanced

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-"""
-nqueens print the coordinates of n queens
-on an nxn grid such that they are all in non-attacking positions
-"""
-
+"""nqueens"""
 
 from sys import argv
 
 if __name__ == "__main__":
-    a = []
+    chessboard = []
     if len(argv) != 2:
         print("Usage: nqueens N")
         exit(1)
-    if argv[1].isdigit() is False:
+    if not argv[1].isdigit():
         print("N must be a number")
         exit(1)
     n = int(argv[1])
@@ -20,39 +16,41 @@ if __name__ == "__main__":
         print("N must be at least 4")
         exit(1)
 
-    # initialize the answer list
     for i in range(n):
-        a.append([i, None])
+        chessboard.append([i, None])
 
-    def already_exists(y):
+    def is_queen_in_column(y):
+        """Check"""
         for x in range(n):
-            if y == a[x][1]:
+            if y == chessboard[x][1]:
                 return True
         return False
 
-    def reject(x, y):
-        if (already_exists(y)):
+    def is_safe(x, y):
+        """safe"""
+        if is_queen_in_column(y):
             return False
         i = 0
-        while(i < x):
-            if abs(a[i][1] - y) == abs(i - x):
+        while i < x:
+            if abs(chessboard[i][1] - y) == abs(i - x):
                 return False
             i += 1
         return True
 
-    def clear_a(x):
+    def clear_chessboard(x):
+        """Clears"""
         for i in range(x, n):
-            a[i][1] = None
+            chessboard[i][1] = None
 
-    def nqueens(x):
+    def solve_nqueens(x):
+        """find"""
         for y in range(n):
-            clear_a(x)
-            if reject(x, y):
-                a[x][1] = y
-                if (x == n - 1):  # accepts the solution
-                    print(a)
+            clear_chessboard(x)
+            if is_safe(x, y):
+                chessboard[x][1] = y
+                if x == n - 1:
+                    print(chessboard)
                 else:
-                    nqueens(x + 1)  # moves on to next x value to continue
+                    solve_nqueens(x + 1)
 
-    # start the recursive process at x = 0
-    nqueens(0)
+    solve_nqueens(0)
