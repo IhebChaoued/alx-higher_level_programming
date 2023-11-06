7034a06974b1c364edd6f09571188ea9c109ea69
Iheb Chaoued <ihebchaoued@gmail.com>
2023-11-06 15:38:23 +0000

    my-int

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -3,12 +3,17 @@
 MyInt class.
 """
 
+
 class MyInt(int):
+    """int"""
     def __new__(cls, *args, **kwargs):
+        """new"""
         return super(MyInt, cls).__new__(cls, *args, **kwargs)
 
     def __eq__(self, other):
+        """eq"""
         return super().__ne__(other)
 
     def __ne__(self, other):
+        """ne"""
         return super().__eq__(other)
