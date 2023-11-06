aa4effef5bc9175447888e10931b1f678eccc663
Iheb Chaoued <ihebchaoued@gmail.com>
2023-11-06 15:33:28 +0000

    updated

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -12,6 +12,3 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-
-    def __str__(self):
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
