1ebaa0db31f982cede38a356e6703a7b3768e0c0
Iheb Chaoued <ihebchaoued@gmail.com>
2023-11-06 15:13:23 +0000

    directly or indirectly

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+class
+"""
+
+def inherits_from(obj, a_class):
+    """
+    determine a class
+    """
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
