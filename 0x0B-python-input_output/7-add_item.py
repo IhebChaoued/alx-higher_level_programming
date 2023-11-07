2868b0435e6ac441d45d6230fb6ddd97bf3cc97c
Iheb Chaoued <ihebchaoued@gmail.com>
2023-11-07 18:01:42 +0000

    add and save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""add all arguments"""
+
+
+import sys
+import os
+
+from 6-load_from_json_file import load_from_json_file
+from 5-save_to_json_file import save_to_json_file
+
+
+def main():
+    """saving"""
+    filename = "add_item.json"
+    my_list = []
+
+    if os.path.isfile(filename):
+        my_list = load_from_json_file(filename)
+
+    my_list.extend(sys.argv[1:])
+
+    save_to_json_file(my_list, filename)
