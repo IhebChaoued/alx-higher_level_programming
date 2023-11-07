4215fe28908c26d7fb34cd1087625657f6049ea4
Iheb Chaoued <ihebchaoued@gmail.com>
2023-11-07 19:40:06 +0000

    add item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-"""add all arguments"""
-
+"""add and save"""
 
 from sys import argv
-
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-
 
 filename = "add_item.json"
 
