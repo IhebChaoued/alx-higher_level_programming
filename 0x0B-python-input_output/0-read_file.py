237de2a7aa844f540e3c07d6ea5e1aa193744af3
Iheb Chaoued <ihebchaoued@gmail.com>
2023-11-07 17:41:56 +0000

    read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def read_file(filename=""):
+    """
+    Read the contents of a text file and print it.
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            print(line, end='')
