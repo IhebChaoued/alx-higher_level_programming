3648fcd533f51bec895a55130303758cc7bdfc6d
Iheb Chaoued <ihebchaoued@gmail.com>
2023-11-07 19:37:27 +0000

    add item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,10 +2,10 @@
 """add all arguments"""
 
 
-import sys
+from sys import argv
 
-from 6-load_from_json_file import load_from_json_file
-from 5-save_to_json_file import save_to_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
 def main():
