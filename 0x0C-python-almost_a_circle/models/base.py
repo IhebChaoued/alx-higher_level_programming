e321aded842a1e410d22403acbd1914ccf90bba9
Iheb Chaoued <ihebchaoued@gmail.com>
2023-11-13 16:01:06 +0000

    updated

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,11 +5,13 @@ import json
 
 
 class Base:
+    """Managing id attribute"""
+
     # Class variable to keep track of objects
     __nb_objects = 0
 
     def __init__(self, id=None):
-        # Initialize instance with an id or create a new id
+        """Initialize instance with an id or create a new id"""
         if id is not None:
             self.id = id
         else:
@@ -18,7 +20,7 @@ class Base:
 
     @staticmethod
     def draw(list_rectangles, list_squares):
-        # Draw using Turtle graphics
+        """Draw using Turtle graphics"""
         turtle.speed(2)
 
         # Draw rectangles
@@ -48,14 +50,14 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        # Return JSON string representation of a list of dictionaries
+        """Return JSON string representation of a list of dictionaries"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        # Save JSON string representation of list_objs to a file
+        """Save JSON string representation of list_objs to a file"""
         filename = cls.__name__ + ".json"
         with open(filename, mode="w", encoding="utf-8") as file:
             if list_objs is None:
@@ -66,14 +68,14 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
-        # Return list of instances from JSON string
+        """Return list of instances from JSON string"""
         if json_string is None or len(json_string) == 0:
             return []
         return json.loads(json_string)
 
     @classmethod
     def load_from_file(cls):
-        # Load list of instances from file
+        """Load list of instances from file"""
         filename = cls.__name__ + ".json"
         try:
             with open(filename, mode="r", encoding="utf-8") as file:
@@ -85,7 +87,7 @@ class Base:
 
     @classmethod
     def create(cls, **dictionary):
-        # Create instance with all attributes already set
+        """Create instance with all attributes already set"""
         if cls.__name__ == "Rectangle":
             dummy = Rectangle(1, 1)
         elif cls.__name__ == "Square":
@@ -97,9 +99,10 @@ class Base:
 
 
 class Rectangle(Base):
-    # Rectangle class
+    """Rectangle class"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
-        # Initialize a Rectangle instance
+        """Initialize a Rectangle instance"""
         super().__init__(id)
         self.width = width
         self.height = height
@@ -107,7 +110,7 @@ class Rectangle(Base):
         self.y = y
 
     def to_dictionary(self):
-        # Return dictionary representation of Rectangle
+        """Return dictionary representation of Rectangle"""
         return {
                 'id': self.id,
                 'width': self.width,
@@ -118,25 +121,26 @@ class Rectangle(Base):
 
 
         class Square(Rectangle):
-            # Square class
+            """Square class"""
+
     def __init__(self, size, x=0, y=0, id=None):
-        # Initialize a Square instance
+        """Initialize a Square instance"""
         super().__init__(size, size, x, y, id)
         self.size = size
 
     @property
     def size(self):
-        # Getter for size
+        """Getter for size"""
         return self.width
 
     @size.setter
     def size(self, value):
-        # Setter for size
+        """Setter for size"""
         self.width = value
         self.height = value
 
     def to_dictionary(self):
-        # Return dictionary representation of Square
+        """Return dictionary representation of Square"""
         return {
                 'id': self.id,
                 'size': self.size,
