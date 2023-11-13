fd62b1905f7cd3ee71f10c5133c5176f5b09ad07
Iheb Chaoued <ihebchaoued@gmail.com>
2023-11-13 21:43:34 +0000

    updated

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -24,8 +24,8 @@ class Square(Rectangle):
     def __str__(self):
         """Return string"""
         return "[Square] ({}) {}/{} - {}".format(
-            self.id, self.x, self.y, self.width
-        )
+                self.id, self.x, self.y, self.width
+                )
 
     def to_dictionary(self):
         """Return the dictionary"""
