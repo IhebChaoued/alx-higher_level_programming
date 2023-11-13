a2c3c4e85e1d7360a4fb6b7c52d5b3a8cd5df33a
Iheb Chaoued <ihebchaoued@gmail.com>
2023-11-13 21:28:42 +0000

    updated

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -79,9 +79,11 @@ class Rectangle(Base):
         return self.__width * self.__height
 
     def display(self):
-        """Print the Rectangle instance with #"""
+        """Print rectangle instance with #"""
+        for _ in range(self.__y):
+            print()
         for _ in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """Return a string representation of the rectangle"""
