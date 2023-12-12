792c95e0d918c24000fcaab2f1e8b923cd9c9c28
Iheb Chaoued <ihebchaoued@gmail.com>
2023-12-12 17:07:05 +0000

    Full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -6,6 +6,6 @@ CREATE TABLE IF NOT EXISTS second_table (
 );
 INSERT INTO second_table
 VALUES (1, "Jhon", "10"),
-(2, "Alex", "3"),
-(3, "Bob", "14"),
-(4, "George", "8");
+	(2, "Alex", "3"),
+	(3, "Bob", "14"),
+	(4, "George", "8");
