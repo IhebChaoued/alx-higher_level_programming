aeda5e36770d2e972d335f821f36a2fa83e788cf
Iheb Chaoued <ihebchaoued@gmail.com>
2023-12-13 14:38:19 +0000

    List specific cities

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- List all cities that can be found in database.
-SELECT id, name FROM cities WHERE state_id (
+SELECT id, name FROM cities WHERE state_id = (
 	SELECT id FROM states WHERE name = "California"
 )
 ORDER BY id;
