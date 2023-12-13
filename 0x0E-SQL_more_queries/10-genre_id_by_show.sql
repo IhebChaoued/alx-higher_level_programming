07f1a0f30b1f06246bb092b4f166d32c21751038
Iheb Chaoued <ihebchaoued@gmail.com>
2023-12-13 14:53:41 +0000

    genre

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- List all shows that have at least one genre linked
 SELECT tv_shows.title, tv_shows_genres.genre_id
 FROM tv_shows
-INNER JOIN tv_shows_genres ON tv_shows_genres.id = tv_shows.id
+INNER JOIN tv_show_genres ON tv_shows_genres.show_id = tv_shows.id
 ORDER BY tv_shows.title ASC, tv_shows_genres.id ASC;
