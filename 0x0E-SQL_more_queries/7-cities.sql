5829a846f6480a5f71c9c590eb78f4a90282be7d
Iheb Chaoued <ihebchaoued@gmail.com>
2023-12-13 14:28:00 +0000

    other things

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,9 +1,8 @@
 -- Create database, table and few other things.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS cities (
 	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	state_id INT NOT NULL,
-	FOREIGN KEY (state_id) REFERENCE states(ID),
+	state_id INT NOT NULL REFERENCE states(ID),
 	name VARCHAR(256) NOT NULL
 );
