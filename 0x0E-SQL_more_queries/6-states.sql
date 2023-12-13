895f74dabcbe91ea950d998872d7cdf80369478a
Iheb Chaoued <ihebchaoued@gmail.com>
2023-12-13 14:16:46 +0000

    Specific ID

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- Create database with table inside.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa:
+CREATE TABLE states (
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL
+);
