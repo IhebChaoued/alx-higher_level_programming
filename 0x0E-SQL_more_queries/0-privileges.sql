21b2b76f64aa253b86a852e56db48cb4e10de1cd
Iheb Chaoued <ihebchaoued@gmail.com>
2023-12-13 13:29:45 +0000

    all privileges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- List all privigel of MySql users.
-SHOW GRANTS FOR '<user_0d_1>'@'<localhost>';
-SHOW GRANTS FOR '<user_0d_2>'@'<localhost>';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
