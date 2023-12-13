9f2bb8451f6937cb49913d73456cba13bdfbf6d3
Iheb Chaoued <ihebchaoued@gmail.com>
2023-12-13 15:01:40 +0000

    no gere

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
 -- List all shows without a genre linked.
 FROM tv_shows
-LEFT JOIN tv_join_genres
+LEFT JOIN tv_show_genres
 ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_show_genres.show_id IS NULL
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
