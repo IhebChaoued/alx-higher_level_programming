12b5958155c9dd8d9d1de4a71818d005058cf020
Iheb Chaoued <ihebchaoued@gmail.com>
2023-12-13 13:54:28 +0000

    Create database with user

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- Create certain database and user
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2_pwd'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2_pw'@'localhost'x;
+FLUSH PRIVILEGES;
