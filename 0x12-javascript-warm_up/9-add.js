5314bd4acc9e5ff7b37e4dd18a59ef315635a7cd
Iheb Chaoued <ihebchaoued@gmail.com>
2024-01-08 22:11:16 +0000

    add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -5,5 +5,4 @@ function add(a, b) {
 }
 const num1 = process.argv[2];
 const num2 = process.argv[3];
-const result = add(num1, num2);
-console.log(result || 'NaN');
+console.log(add(num1, num2) || 'NaN');
