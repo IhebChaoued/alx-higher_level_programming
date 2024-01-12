1017771b320bdd8b25442520877a80c595872b79
Iheb Chaoued <ihebchaoued@gmail.com>
2024-01-12 12:16:29 +0000

    letter a

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Script to list all State objects containing the letter 'a' from the database hbtn_0e_6_usa"""
+"""Script to list all State objects containing the letter 'a'"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -26,6 +26,6 @@ if __name__ == "__main__":
               .order_by(State.id)
               .all())
 
-        print("{}: {}".format(state.id, state.name))
+    print("{}: {}".format(state.id, state.name))
 
     session.close()
