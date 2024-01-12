f4c33ead3df1be41e9f1904c0fa275066e73e5f6
Iheb Chaoued <ihebchaoued@gmail.com>
2024-01-12 12:48:23 +0000

    improve and save

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""State class definition"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from model_city import City
+from model_state import Base
+
+
+class State(Base):
+    """State class"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete-orphan")
