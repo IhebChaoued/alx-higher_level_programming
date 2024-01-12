22fb988c8b4f32b611ea87e294db6b54506fd018
Iheb Chaoued <ihebchaoued@gmail.com>
2024-01-12 13:09:23 +0000

    list all cities

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -4,7 +4,6 @@ import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from relationship_city import Base, City
-from relationship_state import State
 
 
 if __name__ == "__main__":
