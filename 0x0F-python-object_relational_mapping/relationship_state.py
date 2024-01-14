4591d469ae916d9299b185928fdcf60701b66617
Iheb Chaoued <ihebchaoued@gmail.com>
2024-01-14 17:17:37 +0000

    state

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
-"""State class definition"""
-from sqlalchemy import Column, Integer, String, ForeignKey
+"""
+    A script that creates the State “California” with the City “San Francisco”
+    from the database hbtn_0e_100_usa
+    Inherits from SQLAlchemy Base and links to the MySQL table cities
+"""
+
+
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-from model_city import City
-from model_state import Base
+
+Base = declarative_base()
 
 
 class State(Base):
-    """State class"""
+    """
+    An SQL table called states
+    """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+
+    id = Column(Integer, primary_key=True, nullable=False,
+                unique=True, autoincrement=True)
     name = Column(String(128), nullable=False)
-    cities = relationship(
-            "City",
-            backref="state",
-            cascade="all,
-            delete-orphan"
-            )
+    cities = relationship("City", backref="state",
+                          cascade="all, delete-orphan")
