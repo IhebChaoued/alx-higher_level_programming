270f45415addcbda602d89766d71607a856d0d4a
Iheb Chaoued <ihebchaoued@gmail.com>
2024-01-14 17:27:17 +0000

    list all cities

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 """Script to list all City objects and their linked State"""
-import sys
+import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from relationship_city import Base, City
+from relationship_state import Base, State
+from relationship_city import City
+from sys import argv
 
 
 if __name__ == "__main__":
     """Main execution block"""
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-
-    Session = sessionmaker(bind=engine)
+    eng = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
     session = Session()
-
-    for city in session.query(City).order_by(City.id):
+    rows = session.query(City).all()
+    for city in rows:
         print("{}: {} -> {}".format(city.id, city.name, city.state.name))
+    session.close()
