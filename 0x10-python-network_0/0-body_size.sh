db38fcc6bfb3262f771b5be5649a035367ee3f00
Iheb Chaoued <ihebchaoued@gmail.com>
2024-01-25 11:00:46 +0000

    body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # Take a URL, send request and displays the size of the response body in bytes.
 
-url="$1"
-curl -sI "$url" | grep -i Content-Length | awk '{print $2}'
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
