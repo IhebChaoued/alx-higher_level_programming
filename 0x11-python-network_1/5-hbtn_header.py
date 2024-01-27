ec57ffa45bca3f2fa7c215af8ab578c8f3e896a4
Iheb Chaoued <ihebchaoued@gmail.com>
2024-01-27 21:32:16 +0000

    X-Request-Id

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""X-Request-Id"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+
+    print(response.headers.get("X-Request-Id"))
