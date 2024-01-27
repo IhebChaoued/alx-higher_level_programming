4337694170be0e88eb28862f005199535af0b120
Iheb Chaoued <ihebchaoued@gmail.com>
2024-01-27 21:44:57 +0000

    error

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     response = requests.get(url)
 
     if response.status_code >= 400:
-        print(f"Error code: {res.status_code}")
+        print(f"Error code: {response.status_code}")
     else:
         print(f"{response.text}")
